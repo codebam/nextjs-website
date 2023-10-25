08ec104bf4b0820a6d76d4d7521793400e7a2983
Sean Behan <codebam@riseup.net>
2023-10-24 23:12:16 -0400

    add contact and resume pages

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,25 @@
+import fs from "fs";
+import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
+
+const load = async () => {
+	const contents = fs.readFileSync("src/app/resume/resume.md", "utf8");
+	const matterResult = matter(contents);
+	const processedContent = await remark()
+		.use(html)
+		.process(matterResult.content);
+	const contentHtml = processedContent.toString();
+	return { contentHtml, ...matterResult.data };
+};
+
+export default async function Resume() {
+	const data = await load();
+	console.log(data);
+	return (
+		<main>
+			<h1>{data.title}</h1>
+			<div dangerouslySetInnerHTML={{ __html: data.contentHtml }} />
+		</main>
+	);
+}
