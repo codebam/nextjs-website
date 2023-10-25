4c3ba8017cd161ab11a160565842a13204ab5f58
Sean Behan <codebam@riseup.net>
2023-10-25 18:52:15 -0400

    add bootstrap css

--- src/components/headerbar.tsx
+++ src/components/headerbar.tsx
@@ -1,15 +1,32 @@
 import Link from "next/link";
+import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const HeaderBar = () => (
 	<header>
 		<h1>Sean Behan</h1>
-		<Link href="/">Home</Link>
-		<Link href="/posts">Posts</Link>
-		<Link href="/resume">Resume</Link>
-		<Link href="/contact">Contact</Link>
-		<Link href="https://github.com/codebam">GitHub</Link>
-		<Link href="https://p.seanbehan.ca">Pastebin</Link>
-		<Link href="https://term.seanbehan.ca">Terminal</Link>
+		<Button variant="dark" href="/">
+			Home
+		</Button>
+		<Button variant="dark" href="/posts">
+			Posts
+		</Button>
+		<Button variant="dark" href="/resume">
+			Resume
+		</Button>
+		<Button variant="dark" href="/contact">
+			Contact
+		</Button>
+		<Button variant="dark" href="https://github.com/codebam">
+			GitHub
+		</Button>
+		<Button variant="dark" href="https://p.seanbehan.ca">
+			Pastebin
+		</Button>
+		<Button variant="dark" href="https://term.seanbehan.ca">
+			Terminal
+		</Button>
 	</header>
 );
 
