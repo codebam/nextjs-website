35564a8eb1db2c38deab0db4e4ca541a95c29fa2
Sean Behan <codebam@riseup.net>
2023-10-25 19:04:59 -0400

    add header buttons

--- src/components/headerbar.tsx
+++ src/components/headerbar.tsx
@@ -1,32 +1,19 @@
 import Link from "next/link";
 import Button from "react-bootstrap/Button";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 
 const HeaderBar = () => (
 	<header>
-		<h1>Sean Behan</h1>
-		<Button variant="dark" href="/">
-			Home
-		</Button>
-		<Button variant="dark" href="/posts">
-			Posts
-		</Button>
-		<Button variant="dark" href="/resume">
-			Resume
-		</Button>
-		<Button variant="dark" href="/contact">
-			Contact
-		</Button>
-		<Button variant="dark" href="https://github.com/codebam">
-			GitHub
-		</Button>
-		<Button variant="dark" href="https://p.seanbehan.ca">
-			Pastebin
-		</Button>
-		<Button variant="dark" href="https://term.seanbehan.ca">
-			Terminal
-		</Button>
+		<Link href="/">
+			<h1>Sean Behan</h1>
+		</Link>
+		<div>
+			<Button href="/posts">Posts</Button>
+			<Button href="/resume">Resume</Button>
+			<Button href="/contact">Contact</Button>
+			<Button href="https://github.com/codebam">GitHub</Button>
+			<Button href="https://p.seanbehan.ca">Pastebin</Button>
+			<Button href="https://term.seanbehan.ca">Terminal</Button>
+		</div>
 	</header>
 );
 
