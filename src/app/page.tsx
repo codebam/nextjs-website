727af94a7f16daa59ffd688d1b58c6a4fc0e2a3f
Sean Behan <codebam@riseup.net>
2023-10-24 23:46:09 -0400

    add mastodon verification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,6 +41,9 @@ export default async function Home() {
 					<article>{post?.title}</article>
 				</Link>
 			))}
+			<a rel="me" style={{ display: "none" }} href="https://mstdn.ca/@codebam">
+				Mastodon
+			</a>
 		</main>
 	);
 }
