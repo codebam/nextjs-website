11a325d16c70f41b408e43f726684ed913b28928
Sean Behan <codebam@riseup.net>
2023-10-26 16:35:00 -0400

    use navbar from nextui

--- src/components/headerbar.tsx
+++ src/components/headerbar.tsx
@@ -1,43 +1,101 @@
 "use client";
-import { Link, Image } from "@nextui-org/react";
+import {
+	Avatar,
+	Link,
+	Image,
+	Button,
+	Navbar,
+	NavbarBrand,
+	NavbarContent,
+	NavbarItem,
+	NavbarMenuToggle,
+	NavbarMenu,
+	NavbarMenuItem,
+} from "@nextui-org/react";
+import { useState } from "react";
 
-const HeaderBar = () => (
-	<header className="flex flex-wrap">
-		<Link href="/" className="m-4">
-			<Image
-				className="m-2"
-				isBlurred
-				alt="profile"
-				src="https://avatars.githubusercontent.com/u/6035884?v=4"
-				width={"100px"}
-				height={"100px"}
-			/>
-		</Link>
-		<Link className="ml-0" underline="always" href="/">
-			<h1>Sean Behan</h1>
-		</Link>
-		<div className="flex flex-wrap">
-			<Link className="m-2 ml-4" underline="always" href="/posts">
-				Posts
-			</Link>
-			<Link
-				className="m-2"
-				underline="always"
-				href="https://github.com/codebam"
-			>
-				GitHub
-			</Link>
-			<Link className="m-2" underline="always" href="/contact">
-				Contact
-			</Link>
-			<Link className="m-2" underline="always" href="https://p.seanbehan.ca">
-				Pastebin
-			</Link>
-			<Link className="m-2" underline="always" href="https://term.seanbehan.ca">
-				Terminal
-			</Link>
-		</div>
-	</header>
-);
+const HeaderBar = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	return (
+		<Navbar shouldHideOnScroll={true} onMenuOpenChange={setIsMenuOpen}>
+			<NavbarContent>
+				<NavbarMenuToggle
+					aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+					className="sm:hidden"
+				/>
+				<NavbarBrand>
+					<Image
+						className="m-2"
+						isBlurred
+						alt="profile"
+						src="https://avatars.githubusercontent.com/u/6035884?v=4"
+						width={"50px"}
+						height={"50px"}
+					/>
+					<p className="font-bold text-inherit">Sean Behan</p>
+				</NavbarBrand>
+			</NavbarContent>
+
+			<NavbarContent className="hidden sm:flex gap-4" justify="center">
+				<NavbarItem>
+					<Link href="/">Home</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Link href="/posts">Posts</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Link href="https://github.com/codebam">GitHub</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Link href="/contact">Contact</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Link href="https://p.seanbehan.ca">Pastebin</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Link href="https://term.seanbehan.ca">Terminal</Link>
+				</NavbarItem>
+			</NavbarContent>
+			<NavbarContent justify="end"></NavbarContent>
+			<NavbarMenu>
+				<NavbarMenuItem>
+					<Link color={"primary"} className="w-full" href="/" size="lg">
+						Home
+					</Link>
+					<Link color={"primary"} className="w-full" href="/posts" size="lg">
+						Posts
+					</Link>
+					<Link
+						color={"primary"}
+						className="w-full"
+						href="https://github.com/codebam"
+						size="lg"
+					>
+						GitHub
+					</Link>
+					<Link color={"primary"} className="w-full" href="/contact" size="lg">
+						Contact
+					</Link>
+					<Link
+						color={"primary"}
+						className="w-full"
+						href="https://p.seanbehan.ca"
+						size="lg"
+					>
+						Pastebin
+					</Link>
+					<Link
+						color={"primary"}
+						className="w-full"
+						href="https://term.seanbehan.ca"
+						size="lg"
+					>
+						Terminal
+					</Link>
+				</NavbarMenuItem>
+			</NavbarMenu>
+		</Navbar>
+	);
+};
 
 export default HeaderBar;
