057bb1b96edb734042da8918eff3827ed1bd2df9
Sean Behan <codebam@riseup.net>
2023-10-27 02:17:24 -0400

    suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<Script
 				strategy="lazyOnload"
 				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3287237463323384"
